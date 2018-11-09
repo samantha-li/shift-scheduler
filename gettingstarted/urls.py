7474dcbe41cf39bca2dd0ab30e64e78866dfdda5
Samantha Li <spli@princeton.edu>
2018-11-09 15:05:50 -0500

    WIP broken login

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from django.contrib import admin
 admin.autodiscover()
@@ -13,7 +14,7 @@ import hello.views
 urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
     path('accounts/', include('accounts.urls')), # new
-    path('accounts/login', django.contrib.auth.views.login, name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view()),
     url(r'^db', hello.views.db, name='db'),
     url(r'^select', hello.views.select, name='select'),
     url(r'^shift-selection', hello.views.select_shifts, name='shift-selection'),
